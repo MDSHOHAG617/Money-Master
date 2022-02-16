f20276de40cb6d0be0d3787f8130e64fa648f15c
MDSHOHAG617 <95575228+MDSHOHAG617@users.noreply.github.com>
2022-02-17 02:23:51 +0600

    almost Dn!!

--- js/app.js
+++ js/app.js
@@ -2,23 +2,24 @@
 document.getElementById('calculate-tbn').addEventListener('click', function () {
     // food cost
     const foodInput = document.getElementById('food');
-    foodCost = parseFloat(foodInput.value);
+    const foodCost = parseFloat(foodInput.value);
     // rent cost
     const rentInput = document.getElementById('rent');
-    rentCost = parseFloat(rentInput.value);
+    const rentCost = parseFloat(rentInput.value);
     // clothes cost
     const clothesInput = document.getElementById('clothes');
-    clothesCost = parseFloat(clothesInput.value);
+    const clothesCost = parseFloat(clothesInput.value);
     //Total Expenses 
     const expencess = (foodCost + rentCost + clothesCost)
     const totalExpenses = document.getElementById('total-expns');
     totalExpenses.innerText = expencess;
     //Income
     const incomeInput = document.getElementById('income');
-    incomeCost = parseFloat(incomeInput.value);
+    const incomeCost = parseFloat(incomeInput.value);
     //balance
     const balance = document.getElementById('balance');
     balance.innerText = incomeCost - expencess;
+
 
 
 
@@ -28,17 +29,32 @@ document.getElementById('calculate-tbn').addEventListener('click', function () {
 
 //savings
 document.getElementById('save-btn').addEventListener('click', function () {
+    console.log(' clicked')
     //parcentage
-    const savingInput = document.getElementById('savings-perscent');
-    const savingParscentages = parseFloat(savingInput.value);
+    const parscentage = document.getElementById('savings-perscent');
+    const parcentageNum = parseFloat(parscentage.value);
+    //saving amount
+    const incomeInput = document.getElementById('income');
+    const incomeCost = parseFloat(incomeInput.value);
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = incomeCost * (parcentageNum / 100);
+    const lastSavings = incomeCost * (parcentageNum / 100);
+    // console.log(parcentageNum)
+    const foodInput = document.getElementById('food');
+    const foodCost = parseFloat(foodInput.value);
+    // rent cost
+    const rentInput = document.getElementById('rent');
+    const rentCost = parseFloat(rentInput.value);
+    // clothes cost
+    const clothesInput = document.getElementById('clothes');
+    const clothesCost = parseFloat(clothesInput.value);
+    //Total Expenses 
+    const expencess = (foodCost + rentCost + clothesCost)
 
-    //savings amount
-    const savingsAmount = document.getElementById('saving-amount');
-    savingsAmount.innerText = (incomeCost * (savingParscentages / 100));
-
-    //Remaining balance
-    const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = (incomeCost - expencess) - (incomeCost * (savingParscentages / 100));
-    console.log(savingParscentages)
+    const balance = document.getElementById('balance');
+    balance.innerText = incomeCost - expencess;
+    const lastBalance = incomeCost - expencess;
+    const remainigAmount = document.getElementById('remaining-balance');
+    remainigAmount.innerText = lastBalance - lastSavings;
 
 })
